0cca10aa245accb8ad8d1a46415ce9cecc6dca6c
Kayllany04 <kayllany_lima@estudante.sesisenai.org.br>
2024-09-26 17:28:56 -0300

    Descrição do commit

---
+++ Farmacia/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './Header.css'
+
+function Header() {
+
+  return (
+    <div className='kk-container'>
+      
+     
+
+     <div className='kk' > 
+            <img src="farmarcia-logo-pq.png" alt="Logo" />
+     </div>
+
+
+    </div>
+  )
+}
+
+export default Header
